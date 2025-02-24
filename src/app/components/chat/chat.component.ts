780473fef5a4b6f2d284bc5b39e9d70ddce22b8c
Namestruky <fmangom292@gmail.com>
2025-02-24 19:51:35 +0100

    Refactor chat component layout and enhance message handling

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,11 +1,17 @@
-import { Component, ViewChild } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { Message } from '../../interfaces/message';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-import { CreateMLCEngine, MLCEngine  } from "@mlc-ai/web-llm";
+import { CreateMLCEngine, MLCEngine } from "@mlc-ai/web-llm";
+import { max } from 'rxjs';
 
 const SELECTED_ENGINE = "Llama-3.2-1B-Instruct-q4f32_1-MLC";
+
+const MODEL_CONFIG = {
+  maxTokens: 100,
+  temperature: 0.7,
+}
 
 @Component({
   selector: 'app-chat',
@@ -13,18 +19,25 @@ const SELECTED_ENGINE = "Llama-3.2-1B-Instruct-q4f32_1-MLC";
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewInit{
 
-  engine: any;
-  @ViewChild('chatContainer') chatContainer!: HTMLDivElement;
-  
+  infoText: string = "";
+  engine: MLCEngine | null = null;
+  @ViewChild('chatContainer') chatContainer!: ElementRef;
+  @ViewChild('sendButton') sendButton!: ElementRef;
+
   conversacion: Message[] = []
   message: string = '';
 
   constructor() {
-    this.testConversation();
-    //this.initMLCEngine();
+   
   }
+  ngAfterViewInit(): void {
+    //this.testConversation();
+    this.initMLCEngine();
+  }
+
+
 
   /**
    * Initialize the MLC Engine
@@ -34,34 +47,87 @@ export class ChatComponent {
    */
   initMLCEngine() {
     const initProgressCallback = (progress: any) => {
-      console.log("Model loading progress:", progress);
+      this.writeInfoText(`${progress.text}`);
+      if(progress.progress === 1) {
+        this.writeInfoText("Model loaded successfully!");
+        this.sendButton.nativeElement.disabled = false
+
+      }
     }
 
-    this.engine = CreateMLCEngine(SELECTED_ENGINE, { initProgressCallback }).then((engine: MLCEngine) => {
+    CreateMLCEngine(
+      SELECTED_ENGINE,
+      { initProgressCallback },
+      MODEL_CONFIG
+    ).then((engine: MLCEngine) => {
       console.log("Model loaded successfully!");
       this.engine = engine;
+      this.conversacion.push(
+        {
+          author: 'ChatBot',
+          content: '¡Hola! Soy tu asistente virtual. ¿En qué puedo ayudarte?',
+          timestamp: new Date()
+        }
+      )
     }).catch((error: any) => {
       console.error("Error loading model:", error);
     });
   }
 
-  refreshScroll(){
-    this.chatContainer.scrollTop = this.chatContainer.scrollHeight;
+  refreshScroll() {
+    this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
   }
 
-  sendMessage() {
-    this.conversacion.push({author: 'User', content: this.message, timestamp: new Date()});
+  sendMessage_old() {
+    this.conversacion.push({ author: 'User', content: this.message, timestamp: new Date() });
     this.message = '';
     this.simulateRespuesta();
     this.refreshScroll();
   }
 
-  simulateRespuesta(){
-    this.conversacion.push({author: 'ChatBot', content: 'Esto es una respuesta random para simular una respuesta de mi futuro bot', timestamp: new Date()});
+  async sendMessage() {
+
+    if(this.message === '') return;
+    console.log("Sending message:");
+    this.conversacion.push({ author: 'User', content: this.message, timestamp: new Date() });
+    //Se mapean los mensajes de la conversación a un formato que acepta el modelo
+    const messages: any = this.conversacion.map((msg) => {
+      return {
+        content: msg.content,
+        role: msg.author === 'User' ? 'user' : 'assistant'
+      }});
+    console.log("Messages:", messages);
+
+    //Se envía el mensaje al modelo
+    const chunks = await this.engine?.chat.completions.create(
+      {
+        messages: messages,
+        stream: true,
+      }
+    )
+
+    let botMessageIndex = this.conversacion.push({ author: 'ChatBot', content: '', timestamp: new Date() })
+
+    if (chunks) {
+      for await (const chunk of chunks) {
+        if(!(chunk.choices[0].finish_reason === "stop"))
+          this.conversacion[botMessageIndex - 1].content += chunk.choices[0].delta.content;
+        
+        console.log(chunk.choices);
+        this.refreshScroll();
+      }
+    }
+    
+
+    this.message = '';
+  }
+
+  simulateRespuesta() {
+    this.conversacion.push({ author: 'ChatBot', content: 'Esto es una respuesta random para simular una respuesta de mi futuro bot', timestamp: new Date() });
     this.refreshScroll();
   }
 
-  testConversation(){
+  testConversation() {
     this.conversacion.push({
       author: 'User',
       content: 'Hola, necesito ayuda con mi pedido.',
@@ -86,5 +152,9 @@ export class ChatComponent {
       timestamp: new Date()
     });
 
+  }
+
+  writeInfoText(text: string) {
+    this.infoText = text;
   }
 }
